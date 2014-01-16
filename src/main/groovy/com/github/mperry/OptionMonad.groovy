68dbaa32003a1df98747322c232abbcb78fee2c0
Mark Perry <maperry78@yahoo.com.au>
2014-01-17 09:40:13 +1000

    Removed superfluous code

--- src/main/groovy/com/github/mperry/OptionMonad.groovy
+++ src/main/groovy/com/github/mperry/OptionMonad.groovy
@@ -2,35 +2,20 @@ package com.github.mperry
 
 import fj.F
 import fj.data.Option
-import groovy.transform.TypeChecked
-import groovy.transform.TypeCheckingMode
 
 /**
  * Created by MarkPerry on 30/12/13.
  */
-//@TypeChecked
 class OptionMonad extends Monad<Option> {
-
-//    @Override
-//    def <A, B> Option<B> flatMapTyped(Option<A> ma, F<A, Option<B>> f) {
-//        ma.bind(f)
-//    }
 
     @Override
     def <A, B> Option flatMap(Option ma, F<A, Option> f) {
         ma.bind(f)
-//        flatMapTyped(ma, f)
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
     @Override
     def <B> Option<B> unit(B b) {
-       Option.<B>some(b)
+        Option.<B> some(b)
     }
-
-//    @Override
-//    def <A, B> Option<B> map(Option<A> ma, F<A, B> f) {
-//        (Option<B>) super.map(ma, f)
-//    }
 
 }
